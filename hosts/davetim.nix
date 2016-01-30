01e21e36a8983f32171aef0ea2c110359ae5e2a3
Patrick Callahan <patrick.callahan@latitudeengineering.com>
2016-01-30 04:34:41 -0700

    fix broken reference in systempackages for davetim

--- hosts/davetim.nix
+++ hosts/davetim.nix
@@ -55,7 +55,7 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    wget myvim nox
+    wget vimmy nox
   ];
   programs.fish.enable = true;
   security.sudo.enable = true;
