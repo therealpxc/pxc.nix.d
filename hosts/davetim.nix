7b59aa039d9e51ddee3907c9c42f0904b358c9a5
Patrick Callahan <pxcallahan@gmail.com>
2016-02-07 15:50:01 -0700

    add custom nixpkgs to davetim nixpath

--- hosts/davetim.nix
+++ hosts/davetim.nix
@@ -13,6 +13,8 @@
       ./profiles/desktop.nix
     ];
 
+  nix.nixPath = [ "/etc/nixos" "nixos-config=/etc/nixos/configuration.nix" ];
+
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
