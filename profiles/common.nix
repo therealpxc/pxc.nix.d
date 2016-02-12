1e4b9b6f350257b23e173dbd77971c3b1231df46
Patrick Callahan <pxcallahan@gmail.com>
2016-02-11 18:23:05 -0700

    add packages to common profile, custom avahi, vim as default editor

--- profiles/common.nix
+++ profiles/common.nix
@@ -3,6 +3,9 @@
 {
 #  nixpkgs.config.nixPath = config.nixPath ++ [ "/root/.nix-defexpr/channels/nixdev" ];
   nix.nixPath = [ "/etc/nixos" "nixos-config=/etc/nixos/configuration.nix" ];
+  environment.extraInit = ''
+    EDITOR=vim
+  '';
 
   nixpkgs.config.packageOverrides = {
    vimmy = pkgs.vim_configurable.customize {
@@ -40,6 +43,17 @@
               ]; }
        ];
     };
-
+    
+#    avahi = pkgs.callPackage ../nixpkgs/pkgs/development/libraries/avahi {
+#      qt4Support = config.avahi.qt4Support or false;
+#      withLibdnssdCompat = true;
+#    };
   };
+  
+  environment.systemPackages = with pkgs; [
+    vimmy
+    xpra              # in desktop.profile we also include winswitch
+    p7zip
+    utillinuxCurses   # used by oh-my-fish
+  ];
 }
