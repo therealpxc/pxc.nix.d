64c8ef6030fd2f434513ac7ea88fbfedb612da02
Patrick Callahan <pxcallahan@gmail.com>
2016-02-11 18:23:44 -0700

    remove vimmy from profiles/desktop.nix (moved to common.nix), add winswitch and kio plugins

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -15,52 +15,14 @@
   # Set your time zone.
   time.timeZone = "America/Phoenix";
 
-  nixpkgs.config.packageOverrides = { 
-   vimmy = pkgs.vim_configurable.customize {
-       name = "vim";
-
-       vimrcConfig.customRC = ''
-       " set vim shell to bash because Syntastic doesn't like fish and stuff
-       set shell=/run/current-system/sw/bin/bash
-       set number
-       set tabstop=2
-       '';
-       #vimrcConfig.vam.knownPlugins = pkgs.vimPlugins; # optional
-       #vimrcConfig.vam.knownPlugins = pkgs.vimPlugins ++ mypkgs.pkgs.vimPlugins; # optional
-       vimrcConfig.vam.pluginDictionaries = [
-         {  names = [
-              "vim2nix"
-              "sensible"            # sensible defaults
-              "vim-addon-nix"       # vim syntax checking for .nix files
-              "YouCompleteMe"       # better? autocompletion
-              "Syntastic"           # syntax checking
-              "ctrlp"               # fuzzy finder
-              "Tabular"             # alignment guides
-              "Supertab"            # tab completion in insert mode
-              "vim-gitgutter"       # mark changed lines since last commit with a clear visual indicator in the gutter
-              "fugitive"            # some kind of fancy git thing!
-              "UltiSnips"           # fancy snippets
-              "VimOutliner"         # vim outlining; collapse/expand trees like a cool kid
-              "vim-webdevicons"     # cool unicode glyphage
-              #"fireplace"       # Clojure REPL!
-              #"VimClojure"          # Clojure support for vim!
-              #"drgnbrg/vim-redl"    # repl debugging
-              #"ag"
-              #"gitv"
-              "tmux-navigator"
-              ]; }
-
-       ];
-    };
-
-  };
+#  nixpkgs.config.packageOverrides = { 
+#  };
     
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     wget
     which
-    vimmy               # custom vim with plugins and stuff!
     nox                 # nixos package search tool
     super-user-spark    # extremely cool dotfiles manager
     firefox-wrapper     # the wrapper means we get pluginz
@@ -120,6 +82,9 @@
     jdk7    # openjdk 7
     rekonq
     x2goclient
+    winswitch
+    kde5.dolphin-plugins
+    kde5.kio-extras
   ];
   programs.fish.enable = true;
   users.defaultUserShell = "/run/current-system/sw/bin/fish";
