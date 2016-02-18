54ea9c8e3326ac36e6730ff432e24ad37c0845b9
Patrick Callahan <pxcallahan@gmail.com>
2016-02-17 22:34:27 -0700

    add local packages

---
+++ pkgs/my-packages.nix
@@ -0,0 +1,72 @@
+{ config, pkgs, ... }:
+with import <nixpkgs>;
+
+{
+  environment.systemPackages = [
+    vimmy
+  ];
+  
+  nixpkgs.config.packageOverrides = pkgs: rec {  
+   vimmy = pkgs.vim_configurable.customize {
+       name = "vim";
+
+       vimrcConfig.customRC = ''
+       " set vim shell to bash because Syntastic doesn't like fish and stuff
+       set shell=/run/current-system/sw/bin/bash
+       set number
+       set tabstop=2
+       '';
+       #vimrcConfig.vam.knownPlugins = pkgs.vimPlugins; # optional
+       #vimrcConfig.vam.knownPlugins = pkgs.vimPlugins ++ mypkgs.pkgs.vimPlugins; # optional
+       vimrcConfig.vam.pluginDictionaries = [
+         {  names = [
+              "vim2nix"
+              "sensible"            # sensible defaults
+              "vim-addon-nix"       # vim syntax checking for .nix files
+              "YouCompleteMe"       # better? autocompletion
+              "Syntastic"           # syntax checking
+              "ctrlp"               # fuzzy finder
+              "Tabular"             # alignment guides
+              "Supertab"            # tab completion in insert mode
+              "vim-gitgutter"       # mark changed lines since last commit with a clear visual indicator in the gutter
+              "fugitive"            # some kind of fancy git thing!
+              "UltiSnips"           # fancy snippets
+              "VimOutliner"         # vim outlining; collapse/expand trees like a cool kid
+              "vim-webdevicons"     # cool unicode glyphage
+              #"fireplace"       # Clojure REPL!
+              #"VimClojure"          # Clojure support for vim!
+              #"drgnbrg/vim-redl"    # repl debugging
+              #"ag"
+              #"gitv"
+              "tmux-navigator"
+              ]; }
+       ];
+    };
+
+    # needed for px4
+    empy = nixpkgs.pkgs.pythonPackages.buildPythonPackage rec {
+      version = "3.3.2";
+      
+      name = "empy-${version}";
+      
+      src = pkgs.fetchurl {
+        url = "https://pypi.python.org/packages/source/E/EmPy/${name}.tar.gz";
+        sha256 = "177avx6iv9sq2j2iak2il5lxqq0k4np7mpv5gasqmi3h4ypidw4r";
+      };
+
+      meta = {
+        description = "A powerful and robust templating system for Python.";
+        homepage = "https://pypi.python.org/pypi/EmPy";
+        license = licenses.lgpl2;
+        maintainers = [ pxc ];
+      };
+    };
+
+
+    avahi-compat = pkgs.callPackage ../nixpkgs/pkgs/development/libraries/avahi {
+      qt4Support = config.avahi.qt4Support or false;
+      withLibdnssdCompat = true;
+    };
+  };
+}
+
--- profiles/common.nix
+++ profiles/common.nix
@@ -1,54 +1,16 @@
 { config, pkgs, ...}:
 
 {
-#  nixpkgs.config.nixPath = config.nixPath ++ [ "/root/.nix-defexpr/channels/nixdev" ];
+  # import homemade packages and override pkgs
+  imports = [ ../pkgs/my-packages.nix ];
+
+  # allow us to use custom nixpkgs by cloning it into /etc/nixos
   nix.nixPath = [ "/etc/nixos" "nixos-config=/etc/nixos/configuration.nix" ];
+
+  # this is only allowable because vimmy is also installed by default, see below
   environment.extraInit = ''
     EDITOR=vim
   '';
-
-  nixpkgs.config.packageOverrides = {
-   vimmy = pkgs.vim_configurable.customize {
-       name = "vim";
-
-       vimrcConfig.customRC = ''
-       " set vim shell to bash because Syntastic doesn't like fish and stuff
-       set shell=/run/current-system/sw/bin/bash
-       set number
-       set tabstop=2
-       '';
-       #vimrcConfig.vam.knownPlugins = pkgs.vimPlugins; # optional
-       #vimrcConfig.vam.knownPlugins = pkgs.vimPlugins ++ mypkgs.pkgs.vimPlugins; # optional
-       vimrcConfig.vam.pluginDictionaries = [
-         {  names = [
-              "vim2nix"
-              "sensible"            # sensible defaults
-              "vim-addon-nix"       # vim syntax checking for .nix files
-              "YouCompleteMe"       # better? autocompletion
-              "Syntastic"           # syntax checking
-              "ctrlp"               # fuzzy finder
-              "Tabular"             # alignment guides
-              "Supertab"            # tab completion in insert mode
-              "vim-gitgutter"       # mark changed lines since last commit with a clear visual indicator in the gutter
-              "fugitive"            # some kind of fancy git thing!
-              "UltiSnips"           # fancy snippets
-              "VimOutliner"         # vim outlining; collapse/expand trees like a cool kid
-              "vim-webdevicons"     # cool unicode glyphage
-              #"fireplace"       # Clojure REPL!
-              #"VimClojure"          # Clojure support for vim!
-              #"drgnbrg/vim-redl"    # repl debugging
-              #"ag"
-              #"gitv"
-              "tmux-navigator"
-              ]; }
-       ];
-    };
-    
-#    avahi = pkgs.callPackage ../nixpkgs/pkgs/development/libraries/avahi {
-#      qt4Support = config.avahi.qt4Support or false;
-#      withLibdnssdCompat = true;
-#    };
-  };
   
   environment.systemPackages = with pkgs; [
     vimmy
