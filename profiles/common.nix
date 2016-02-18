f77455e1d2b8e161eb1b4917d6ebd9a382d80450
Patrick Callahan <pxcallahan@gmail.com>
2016-02-18 14:05:43 -0700

    move some service configs and packages from desktop profile to common

--- profiles/common.nix
+++ profiles/common.nix
@@ -14,12 +14,66 @@
 
   services.nfs.server.enable = true;
   services.sshd.enable = true;
+  programs.fish.enable = true;
+  users.defaultUserShell = "/run/current-system/sw/bin/fish";
+  security.sudo.enable = true;
+  services.locate.enable = true;
+  
+  services.samba.enable = true;
+  services.samba.nsswins = true;
+  
+  virtualisation.libvirtd.enable = true;
+  virtualisation.docker.enable = true;
+  virtualisation.lxc.enable = true;
   
   environment.systemPackages = with pkgs; [
     vimmy
     xpra              # in desktop.profile we also include winswitch
     p7zip
-    utillinuxCurses   # used by oh-my-fish
+    cowsay
+    fuse-7z-ng
+    sshfsFuse
     nfs-utils
+    vagrant
+    linuxPackages.virtualbox
+    
+    gitAndTools.gitFull
+    gitAndTools.gitflow
+    gitAndTools.hub
+    gitAndTools.git-annex
+    gitAndTools.git-remote-hg
+    super-user-spark    # extremely cool dotfiles manager
+
+    wget
+    which
+    nox                 # nixos package search tool
+    dvtm
+    dtach
+    abduco
+    figlet              # command-line tool for rendering stylized text in ascii-art
+
+    # used by my fancy fish config
+    grc
+    utillinuxCurses   # used by oh-my-fish
+    silver-searcher
+    byobu
+    tmux
+    python35Packages.powerline
+    fzf
+    fasd
+    mercurialFull
+    subversion
+    keychain
   ];
+  
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.pxc = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = [ "users" "docker" "libvirtd" ];
+  };
+  users.groups = {
+    wheel = { members = [ "pxc" ]; };
+  };
+
 }
--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -21,23 +21,12 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    wget
-    which
-    nox                 # nixos package search tool
-    super-user-spark    # extremely cool dotfiles manager
+
     firefox-wrapper     # the wrapper means we get pluginz
-    figlet              # command-line tool for rendering stylized text in ascii-art
     kde4.yakuake        # quake-style terminal for KDE
     lighttable          # lighttable editor
     leiningen           # Clojure project + dependency manager
-    gitAndTools.gitFull
-    gitAndTools.gitflow
-    gitAndTools.hub
-    gitAndTools.git-annex
-    gitAndTools.git-remote-hg
-    silver-searcher
-    byobu
-    tmux
+
     vimPlugins.vim-addon-vim2nix
     vimPlugins.vim-addon-manager
     nodePackages.bower2nix
@@ -46,19 +35,9 @@
     python2nix
     egg2nix
     kde5.konversation
-    fzf
-    fasd
-    mercurialFull
-    subversion
-    keychain
     python27Packages.docker_compose
     kde5.breeze
     #man_db
-    #pypyPackages.powerline
-    python35Packages.powerline
-    dvtm
-    dtach
-    abduco
     kde5.plasma-pa
     kde5.kdeplasma-addons
     smbnetfs
@@ -66,17 +45,13 @@
     cifs_utils
     kde4.cantor
     s3fs
-    sshfsFuse
     google-drive-ocamlfuse
-    fuse-7z-ng
     kde5.kdbusaddons
     glxinfo
-    cowsay
     atom
     dwm
     i3
     kde5.okular
-    grc
     eclipses.eclipse_sdk_451
     jdk     # openjdk 8
     jdk7    # openjdk 7
@@ -88,44 +63,20 @@
     i3status
     dmenu
   ];
-  programs.fish.enable = true;
-  users.defaultUserShell = "/run/current-system/sw/bin/fish";
-  security.sudo.enable = true;
-  services.locate.enable = true;
+
   hardware.pulseaudio.enable = true;
-
   services.xserver.windowManager.i3.enable = true;
-
-  virtualisation.libvirtd.enable = true;
-  virtualisation.docker.enable = true;
-  virtualisation.lxc.enable = true;
-  
-    
   services.avahi.enable = true;
   services.avahi.nssmdns = true;
-  # List services that you want to enable:
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-  
-  services.samba.enable = true;
-  services.samba.nsswins = true;
-
   services.telepathy.enable = true;
 
   # this may be dangerous! this uses a kms-based console instead of getty
   # it may make my console awesome... it might fuck me
   services.kmscon.enable = true;
   
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.extraUsers.pxc = {
-    isNormalUser = true;
-    uid = 1000;
-    extraGroups = [ "users" "docker" "libvirtd" ];
-  };
-  users.groups = {
-    wheel = { members = [ "pxc" ]; };
-  };
 
   services.xserver.enable = true;
   #services.xserver.autorun = false; # i think we're good?
