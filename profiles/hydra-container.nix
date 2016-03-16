b98f4d3cf47af47c2059ab77319a6343d9a531ff
Patrick Callahan <pxcallahan@gmail.com>
2016-03-16 16:11:44 -0700

    separate hydra config from containerization

--- profiles/hydra-container.nix
+++ profiles/hydra-container.nix
@@ -1,50 +1,23 @@
 { config, pkgs, ... }:
 {
-  containers.hydra = { config =
-    { config, pkgs, ... }:
-    {
-      # run nixos-15.09 inside the container
 
-      # hydra container config lives in here
-      services.ntp.enable = false;
-      services.openssh.allowSFTP = false;
-      services.openssh.passwordAuthentication = false;
+  #users.extraUsers = {
+  #    hydra = { extraGroups = [ "hydra" ]; };
+  #    hydra-queue-runner = { extraGroups = [ "hydra" ]; };
+  #};
 
-      users = {
-        mutableUsers = false;
-        users.root.openssh.authorizedKeys.keyFiles = [ "~pxc/.ssh/id_rsa.pub" ];
-      };
+#  nix.trustedUsers = [ "@hydra" ];
 
-      imports = [ ../hydra/hydra-module.nix ];
-
-      networking.firewall.allowedTCPPorts = [ config.services.hydra.port ];
-
-      nix = {
-#        nixPath = [ "nixpkgs=https://github.com/nixos/nixpkgs-channels/archive/nixos-15.09.tar.gz" ];
-        nixPath = [ "nixpkgs=https://github.com/nixos/nixpkgs-channels/archive/nixos-16.03-beta.tar.gz" ];
-        distributedBuilds = true;
-        useChroot = true;
-        buildMachines = [
-
-        ];
-        extraOptions = "auto-optimise-store = true";
-      };
-
-      services.hydra = {
-        enable = true;
-        hydraURL = "http://hydra.rex.latitudeengineering.com";
-        notificationSender = "hydra@latitudeengineering.com";
-        #listenHost = "192.168.0.216";
-        port = 8080;
-        extraConfig = "binary_cache_secret_key_file = /etc/nix/hydra.rex.latitudeengineering.com-1/secret";
-      };
-
-      services.postgresql = {
-        package = pkgs.postgresql94;
-        dataDir = "/var/db/postgresql-${config.services.postgresql.package.psqlSchema}";
-      };
-
-    };
+  containers.chydra = {
+    config = import ./hydra.nix;
+    # more container options
     autoStart = true;   # run the container when the host system boots
+    privateNetwork = true;
+    hostAddress = "192.168.100.10";
+    localAddress = "192.168.100.11";
   };
+
+  networking.nat.enable = true;
+  networking.nat.internalInterfaces = ["ve-+"];
+  networking.nat.externalInterface = "enp0s31f6";
 }
