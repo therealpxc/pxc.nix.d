1cd7cf890a7349e89589c467f6ca857c1590f29d
Patrick Callahan <pxcallahan@gmail.com>
2016-03-30 16:10:18 -0700

    update X configuration for nvidia+multimonitor

--- hosts/rex.nix
+++ hosts/rex.nix
@@ -50,6 +50,40 @@
   # rex has an NVIDIA GTX 750 Ti
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.driSupport32Bit = true;
+  #hardware.bumblebee.enable = true;
+  boot.blacklistedKernelModules = [ "i915" ];
+  services.xserver.monitorSection = ''
+    # HorizSync source: edid, VertRefresh source: edid
+  #  Identifier     "Monitor0"
+    VendorName     "Unknown"
+    ModelName      "Ancor Communications Inc VS24A"
+    HorizSync       30.0 - 83.0
+    VertRefresh     50.0 - 61.0
+    Option         "DPMS"
+  '';
+
+  services.xserver.deviceSection = ''
+    #Identifier     "Device0"
+    Driver         "nvidia"
+    VendorName     "NVIDIA Corporation"
+    BoardName      "GeForce GTX 750 Ti"
+  '';
+  
+  services.xserver.defaultDepth = 24;
+  services.xserver.screenSection = ''
+    #Identifier     "Screen0"
+    Device         "Device0"
+    Monitor        "Monitor0"
+    DefaultDepth    24
+    Option         "Stereo" "0"
+    Option         "nvidiaXineramaInfoOrder" "DFP-0"
+    Option         "metamodes" "GPU-67c27bbf-8249-62c5-6a05-797140c598bc.DVI-I-1: nvidia-auto-select +0+0, GPU-67c27bbf-8249-62c5-6a05-797140c598bc.DP-0: nvidia-auto-select +3840+0, GPU-67c27bbf-8249-62c5-6a05-797140c598bc.HDMI-0: nvidia-auto-select +1920+0"
+    Option         "MultiGPU" "Off"
+    Option         "SLI" "off"
+    Option         "BaseMosaic" "on"
+  '';
+
+
 
   # rex has bluetooth + wifi
   hardware.bluetooth.enable = true;
