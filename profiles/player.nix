e78028a48cddbc6850fca869177589affbab2206
Patrick Callahan <pxcallahan@gmail.com>
2016-05-07 18:26:19 -0700

    add standalone emulators, games, toys, and drivers

--- profiles/player.nix
+++ profiles/player.nix
@@ -50,6 +50,46 @@ in
     # modular game console emulator + backends
     retroarch
 
+    # misc emulators
+    mupen64plus
+    #wxmupen64plus     # broken as of 2016-05-07
+    pcsx2
+    higan
+    fceux
+    zsnes
+    snes9x-gtk
+    gensgs
+    yabause
+    atari800
+    hatari
+    ataripp
+    stella
+    dolphinEmuMaster
+    #mess           # appears broken as of 2016-05-07
+    residualvm      # LucasArts adventure games
+    
+    # hardware peripheral drivers and services
+    cwiid
+    xwiimote
+    #xboxdrv
+    #python27Packages.ds4drv  # this got removed from nixpkgs???
+    qjoypad
+    #antimicro      # broken as of 2016-05-07
+
+    # games & toys
+    game-music-emu
+    freedink        # Dink Smallwood!
+    hawkthorne      # Community game!
+    kde4.ksirk      # Risk
+    n2048
+    openmw          # Morrowind
+    openra          # Command & Conquer: Red Alert
+    superTux
+    wesnoth
+    widelands
+    warzone2100
+    xonotic
+
   ] ++ kodiPlugins ++ retroPlugins;
 
   # 32-bit support for WINE and Steam games
