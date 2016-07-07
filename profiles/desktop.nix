c09f2df901f6437044d32b56a09703be6a9e3b43
Patrick Callahan <pxcallahan@gmail.com>
2016-07-06 20:08:39 -0700

    add cutegram to desktop profile

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -13,6 +13,7 @@
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     firefox
+    cutegram
     kde4.yakuake        # quake-style terminal for KDE
 
     vimPlugins.vim-addon-manager
