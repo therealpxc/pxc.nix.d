716f788870355e727223cfafc3c0ae8dbebb5754
Patrick Callahan <pxcallahan@gmail.com>
2016-11-19 18:21:07 -0700

    add xkbOptions line to enable xcompose in module

--- modules/xcompose/default.nix
+++ modules/xcompose/default.nix
@@ -1,12 +1,16 @@
 { config, lib, ... }: with lib;
 {
-  config.environment = mkIf config.services.xserver.enable {
-    etc.XCompose.source = ./pointless-extended.xcompose;
-    variables.GTK_IM_MODULE = "xim";
-    variables.QT4_IM_MODULE = "xim";
+  config = mkIf config.services.xserver.enable {
+    environment = {
+      etc.XCompose.source = ./pointless-extended.xcompose;
+      variables.GTK_IM_MODULE = "xim";
+      variables.QT4_IM_MODULE = "xim";
 
-    # xim is an alias to compose with qt5
-    variables.QT_IM_MODULE = "compose";
-    variables.XCOMPOSEFILE = "/etc/XCompose";
+      # xim is an alias to compose with qt5
+      variables.QT_IM_MODULE = "compose";
+      variables.XCOMPOSEFILE = "/etc/XCompose";
+    };
+
+    services.xserver.xkbOptions = "compose:caps";
   };
 }
