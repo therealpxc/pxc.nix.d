f8a18dc7c612d4ec8fb680aca077a134e952c3e8
Patrick Callahan <pxcallahan@gmail.com>
2017-12-13 07:38:13 -0700

    remove some stuff from 'workstation' role

--- roles/workstation.nix
+++ roles/workstation.nix
@@ -3,27 +3,14 @@
 {
   imports = [
     ./desktop.nix
-    ./virtualization.nix
   ];
 
-  programs.adb.enable = true;
-  services.udev.packages = with pkgs; [
-    android-udev-rules
-  ];
-  programs.mosh.enable = true;
-  
+
   environment.systemPackages = with pkgs; [
-    adb-sync
-    (heimdall.overrideAttrs (oldAttrs: rec {
-      withGUI = true;
-    }))
-
     lighttable              # lighttable editor
     leiningen               # Clojure project + dependency manager
     glxinfo
     atom
-    eclipses.eclipse-sdk
-    #eclipses.eclipse-scala-sdk # based on Eclipse 4.4.1
     scala     # scala (2.12.2 as of 2017-05-14)
     scalafmt  # scala formatter
     sbt       # ‘scala build tool’
@@ -32,7 +19,6 @@
     jdk     # openjdk 8
 
     # python development tools
-    python3
     python35Packages.pew
     python35Packages.virtualenv
 
